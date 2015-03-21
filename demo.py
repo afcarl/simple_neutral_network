48eb3a0888978155ce60894a9dcf4d5d8783ce19
zizhaozhang <mr.zizhaozhang@gmail.com>
2015-03-21 17:14:33 -0400

    dataset

--- demo.py
+++ demo.py
@@ -1,13 +1,13 @@
 import nn
-import synthetic_data
+import circle_data
 from sklearn.metrics import accuracy_score
 from sklearn import cross_validation
 from sklearn import datasets
 
-# X, y = synthetic_data.genData(10000, ifplot=0)
+d = circle_data.genData(10000)
 
-ir = datasets.load_iris()
-X, y = ir.data, ir.target
+# ir = datasets.load_iris()
+X, y = d.data, d.target
 
 clf = nn.neutral_network(10, maxiter=700)
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.5)
